0ac1045ccfc1ab6c44e7f135bfafab28803d2b89
Jorge Medina <jim@umail.ucsb.edu>
2021-06-01 23:29:03 -0700

    implemented push

--- heap.cpp
+++ heap.cpp
@@ -8,13 +8,40 @@ using std::cout;
 // Pushes a value into the heap, then ensures
 // the heap is correctly arranged
 void Heap::push(int value){
-
+    vdata.push_back(value);
+    
+    int i = vdata.size()-1;
+    int parent = (i-1)/2;
+   
+   while( parent >= 0 && value < vdata.at(parent))
+    {
+        vdata.at(i) = vdata.at(parent);
+        vdata.at(parent) = value;
+        
+        i = parent;
+        parent = (parent-1)/2;
+       
+    }
 }
 
 // Pops the minimum value off the heap
 // (but does not return it), then ensures
 // the heap is correctly arranged
 void Heap::pop(){
+    //int lastKey = vdata.at(vdata.size()-1);
+    //vdata.at(vdata.size()-1) = vdata.at(0);
+    //vdata.at(0) = lastKey;
+    if(vdata.empty()){return;}
+
+    vdata.at(0) = vdata.at(vdata.size()-1);
+    vdata.pop_back();
+    
+    int i = 0;
+    int lChild = (2*i+1);
+    int rChild = (2*i+1);
+
+
+
 
 }
 
@@ -32,7 +59,7 @@ int Heap::top(){
 
 // Returns true if the heap is empty, false otherwise
 bool Heap::empty(){
-  if(vdata.empty() ){ return true;
+  if(vdata.empty() ){ return true;}
   
   else
       return false;
